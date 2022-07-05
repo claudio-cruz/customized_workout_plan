3606c9ad97b3f4bbfd71274e365cf7a12cca9fdd
Claudio Cruz <claudiocruz1989@gmail.com>
2022-07-05 19:10:25 +0000

    Create the validate_name function to validate the name input

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+from email.policy import strict
+
+
 def get_user_data():
     """
     Get the name, height, weight, and gender input date from the user.
@@ -6,8 +9,13 @@ def get_user_data():
     """
     while True:
         print('Please provide us with the following requested data so we can provide you with your ideal weight and your customized workout plan.')
+        
         print('Please enter your name.')
-        user_name = input('Enter your name:\n')
+        user_name = input('Enter your name:\n\n')
+
+        if validate_name(user_name):
+            print(f'Welcome {user_name.title()}!The data you provided is valid.\n')
+        else: continue
 
         print('Please enter your height in cm.')
         user_height = input('Enter your height in cm:\n')
@@ -18,6 +26,22 @@ def get_user_data():
         print('Please tell us your gender.')
         user_weight = input('Enter your gender:\n')
 
-        if validate_data(value):
-            print(f'Welcome {user_name.title()}!The data you provided is valid.')
+        
 
+def validate_name(value):
+    """
+    Validates the name input provided by the user.
+    Returns a message if the input is not valid and repeats the loop.
+    Returns a message if the input is valid and proceeds with the rest of the loop.
+    """
+    try:
+        if len(value) > 15: 
+            raise ValueError
+            print('try again')
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again.\n')
+        return False
+
+    return True
+        
+get_user_data()
