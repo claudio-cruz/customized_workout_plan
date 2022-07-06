1c28af31faae1a0746dc04d5d7616f1b53006df3
Claudio Cruz <claudiocruz1989@gmail.com>
2022-07-06 03:59:39 +0000

    Add validate_height_weight function

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from email.policy import strict
+from unittest import expectedFailure
+
+from tomlkit import integer
 
 
 def get_user_data():
@@ -10,23 +13,32 @@ def get_user_data():
     while True:
         print('\nPlease provide us with the following requested data so we can provide you with your ideal weight and your customized workout plan.\n')
         
-        user_name = input('Please enter your name:\n')
-
+        """user_name = input('Please enter your name:\n')
         if validate_name(user_name):
             print(f'Welcome {user_name.title()}! Your name has been submitted.\n')
+        else: continue"""
+
+        input_height = input('Please enter your height in cm: \n')
+        user_height = int(input_height)
+        print(type(user_height))
+        if validate_height_weight(user_height):
+            print('Your height has been submitted.\n')
         else: continue
 
-        print('Please enter your height in cm.')
-        user_height = input('Enter your height in cm: ')
-
-        print('Please enter your weight in kg.')
-        user_weight = input('Enter your weight in kg: ')
+        input_weight = input('Please enter your weight in kg: \n')
+        try:
+            user_weight = int(input_weight)
+        except:
+            print('no')
+        if validate_height_weight(user_weight):
+            print('Your weight has been submitted.\n')
+        else: continue
 
         print('Please tell us your gender.')
         user_weight = input('Enter your gender: ')
+        break
 
         
-
 def validate_name(value):
     """
     Validates the name input provided by the user.
@@ -43,5 +55,23 @@ def validate_name(value):
         return False
 
     return True
-        
+
+
+def validate_height_weight(value):
+    """
+    Validates the height and the weight input provided by the user.
+    If the input is not a valid number prints a message and repeats the loop.
+    Returns a message if the input is valid and proceeds with the rest of the loop.
+    """
+    try:
+        if type(value) != int or len(str(value)) != 3:
+            raise ValueError
+
+    except ValueError as e:
+        print('\nInvalid input:')
+        return False
+
+    return True
+
+
 get_user_data()
