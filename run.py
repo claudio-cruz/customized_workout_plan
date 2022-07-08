6f5cc5ef04abef2d93e5056fa61f79e909cf57b9
Claudio Cruz <claudiocruz1989@gmail.com>
2022-07-08 15:16:29 +0000

    Create calculate_bmi function to calculate user bmi and print the bmi status

--- run.py
+++ run.py
@@ -20,13 +20,13 @@ def get_user_data():
         else: continue
 
         global user_height
-        user_height = input('Please enter your height in cm:\n')
+        user_height = float(input('Please enter your height in cm:\n'))
         if validate_height_weight(user_height):
             print('Your height has been submitted.\n')
         else: continue
 
         global user_weight
-        user_weight = input('Please enter your weight in kg:\n')
+        user_weight = float(input('Please enter your weight in kg:\n'))
         if validate_height_weight(user_weight):
             print('Your weight has been submitted.\n')
         else: continue
@@ -66,7 +66,7 @@ def validate_height_weight(value):
     Retur a message if the input is valid and proceeds with the rest of the loop.
     """
     try:
-        if int(value) not in range(40, 250):
+        if value not in range(40, 250):
             raise ValueError
 
     except ValueError as e:
@@ -108,9 +108,40 @@ def calculate_ideal_weight(height, gender):
         ideal_weight = 45.5 + (0.91 * (height - 152.4))
         print(f'Your ideal weight is {round(ideal_weight, 1)}')
         return ideal_weight
-        
+
+
+def calculate_bmi(weight, height):
+    """
+    Calculate the BMI (body mass index).
+    Return a message if the user is underweight, healthy weight, over weith, or obese.
+    Print a message with the BMI chart for men and women.
+    """
+    global user_bmi
+    user_bmi = weight / (height / 100)**2
+
+    if user_bmi < 18.5:
+        print(f'{user_name.title()}, your BMI is {round(user_bmi, 1)} which means that you are underweight.\n')
+    
+    elif user_bmi <= 24.9:
+        print(f'{user_name.title()}, your BMI is {round(user_bmi, 1)} which means that you you have a healthy weight.\n')
+    
+    elif user_bmi <= 29.9:
+        print(f'{user_name.title()}, your BMI is {round(user_bmi, 1)} which means that you are overweight.\n')
+    
+    elif user_bmi >= 30.0:
+        print(f'{user_name.title()}, your BMI is {round(user_bmi, 1)} which means that you are obese.\n')
+    
+    bmi_chart_dic = {
+        'Below 18.5': 'Underweight',
+        '18.5-24.9': 'Healthy weight',
+        '25.0-29.9': 'Overweight',
+        '30.0 and above': 'Obesity'
+    }
+    print('BMI Chart for Men and Women:')
+    for key in bmi_chart_dic:
+         print(key, '->', bmi_chart_dic[key])
 
 
 get_user_data()
-calculate_ideal_weight(int(user_height), user_gender)
-print(ideal_weight)
+calculate_ideal_weight(user_height, user_gender)
+calculate_bmi(user_weight, user_height)
